e4b6b591997dff6dd94e8659192d9b2a3c0e9ca0
Kurt Almadrones <140587090+justjstine@users.noreply.github.com>
2025-03-04 20:20:50 +0800

    Update ContentRating.java

--- src/data/ContentRating.java
+++ src/data/ContentRating.java
@@ -2,16 +2,16 @@ package data;
 
 import javafx.beans.property.SimpleStringProperty;
 
-public class ContentRating {
+public class ContentRating { // Private fields to store content rating ID and classification
     private final SimpleStringProperty contentratingid;
     private final SimpleStringProperty classification;
 
-    public ContentRating(String crid, String cf){
+    public ContentRating(String crid, String cf){ // Initialize content rating ID and classification fields
         this.contentratingid = new SimpleStringProperty(crid);
         this.classification = new SimpleStringProperty(cf);
     }
 
-    public String getContentratingid(){
+    public String getContentratingid(){  // Getter methods
         return contentratingid.get();
     }
     public String getClassification() {
